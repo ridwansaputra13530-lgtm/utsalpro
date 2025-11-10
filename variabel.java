df391ae62ca780eacb2372694ac2838b4ceb8adf
ridwansaputra13530-lgtm <ridwansaputra13530@gmail.com>
2025-11-10 16:31:49 +0700

    ridwansaputra

---
+++ variabel.java
@@ -0,0 +1,22 @@
+public class Variabel {
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        String nama = "Ridwan Saputra";
+        String tempatTanggalLahir = "BOGOR, 30 Januari 2007";
+        String jenisKelamin = "Laki-laki";
+        String alamat = "Jl.Curug Luhur Indah TR02/04, BOGOR";
+        String noHp = "085893616533s";
+        double penghasilan = 192500000.0; // bisa bebas sesuai perintah
+
+        // Output
+        System.out.println("Assalamu’alaikum");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama Lengkap         : " + nama);
+        System.out.println("Tempat & Tanggal Lahir : " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin        : " + jenisKelamin);
+        System.out.println("Alamat Lengkap       : " + alamat);
+        System.out.println("No HP                : " + noHp);
+        System.out.println("Penghasilan Sebulan  : Rp" + penghasilan);
+    }
+}
